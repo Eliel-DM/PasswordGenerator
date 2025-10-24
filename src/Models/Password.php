02ee5143eec0e0d9002d9beaccbdc03b90eef7c9
Eliel Dias Matos <elieldiasmatos@pop-os.bbrouter>
2025-10-24 12:05:37 -0300

    Declarando corretamente os atributos bolleanos, e inserindo o contrutor dentro da classe

--- src/Models/Password.php
+++ src/Models/Password.php
@@ -1,31 +1,38 @@
-<?php
+<?php 
 
 declare (strict_types=1);
 namespace App\Models;
 
-Class Password {
+Class Password 
+{
     private int $lenth;
-    private bollean $numbers;
-    private bollean $symbols;
-    private bollean $includeLowerCase;
-    private bollean $includeUpperCase;
+    private bool $numbers;
+    private bool $symbols;
+    private bool $includeLowerCase;
+    private bool $includeUpperCase;
     private string $password;
+
+    function __contruct (
+        int $lenth,
+        bool $numbers,
+        bool $symbols,
+        bool $includeLowerCase,
+        bool $includeUpperCase
+    ){
+        $this->lenth = $lenth;
+        $this->numbers = $numbers;
+        $this->symbols = $symbols;
+        $this->includeLowerCase = $includeLowerCase;
+        $this->includeUpperCase = $includeUpperCase;
 }
 
-function __contruct (
-    int $lenth,
-    bollean $numbers,
-    bollean $symbols,
-    bollean $includeLowerCase,
-    bollean $includeUpperCase
-){
-    $this->lenth = $lenth;
-    $this->numbers = $numbers;
-    $this->symbols = $symbols;
-    $this->includeLowerCase = $includeLowerCase;
-    $this->includeUpperCase = $includeUpperCase;
 }
+
+
 
 //Criar a função para criar a senha, de acordo com os requisitos instanciados no inde;
 //Criar os getters e setters;
+
+?>
+
 
